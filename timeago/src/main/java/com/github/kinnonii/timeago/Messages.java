f42ee7890062b6db98da0f228e5005123bf6f6d8
Fohz <69390423+Fohz@users.noreply.github.com>
2020-08-08 15:39:16 +0200

    Update Messages.java

--- timeago/src/main/java/com/github/kinnonii/timeago/Messages.java
+++ timeago/src/main/java/com/github/kinnonii/timeago/Messages.java
@@ -29,7 +29,7 @@ import java.util.ResourceBundle;
  */
 public class Messages {
 
-	private static final String BUNDLE_NAME = "com.github.kinnonii.timeago.messages"; //$NON-NLS-1$
+	private static final String BUNDLE_NAME = "com.github.kinnonii.TimeAgo.messages"; //$NON-NLS-1$
 
 	private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle
 			.getBundle(BUNDLE_NAME);
